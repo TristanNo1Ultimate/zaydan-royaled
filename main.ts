f6dabb606408f3f95405bf2bba8a0b10d8a6ab2c
TristanNo1Ultimate <75755295+TristanNo1Ultimate@users.noreply.github.com>
2020-12-21 19:45:51 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,65 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Zaydan.vy = -100
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    Zaydan.vx = -100
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    Zaydan.vx = 0
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    Zaydan.vx = 0
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    Zaydan.vx = 100
+})
+let Zaydan: Sprite = null
+Zaydan = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . b b b . . 8 . . . 
+    . . . . . b b b . b . . 8 . . . 
+    . . . . . b . . . b b . 8 . . . 
+    . . . . . b 2 . 2 . b . 8 . . . 
+    . . . . . b . . . . b . 8 . . . 
+    . . . . . b b b b b b 8 8 8 8 . 
+    . . . . . b b b b b b . 8 . . . 
+    . 7 7 7 7 b b b b b b 7 7 7 . . 
+    . . . . . b b b b b b . . . . . 
+    . . . . . b b b b b b . . . . . 
+    . . . . . b b b b b b . . . . . 
+    . . . . . b b . . b b . . . . . 
+    . . . . . b b . . b b . . . . . 
+    . . . . . b b . . b b . . . . . 
+    `, SpriteKind.Player)
+Zaydan.ay = 100
+let KingDuck = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . b . b b . b . . . . . . 
+    . . . . . b b b b . . . . . . . 
+    . . . . 5 2 2 2 2 . . . . . . . 
+    . . . 5 5 2 d d 2 . . . . . . . 
+    . . 5 5 5 2 d d 2 . . . . . . . 
+    . . . 5 5 2 d d 2 . . . . . . . 
+    . . . . 5 2 2 2 2 . . . . . . . 
+    . . . . . 9 d d 9 . . . . . . . 
+    . . . . . 9 d d 9 . . . . . . . 
+    . . . . . 9 d d 9 . . . . . . . 
+    . . . . . 9 d d 9 . . . . . . . 
+    . . . . . 9 d d 9 . . . . . . . 
+    . . . . . 9 d d 9 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)
+Zaydan.setFlag(SpriteFlag.StayInScreen, true)
+scene.setBackgroundColor(9)
+tiles.setTilemap(tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010101`, img`
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    2 2 2 2 2 2 2 2 2 2 
+    `, [myTiles.transparency16,sprites.builtin.forestTiles2], TileScale.Sixteen))
